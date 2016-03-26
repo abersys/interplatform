ca462d31bb1569effb9c8db78260f702bb6ba534
akodanev <akodanev@gmail.com>
2016-03-26 17:13:52 +0300

    include/common.h: make variadic macro more portable
    add a white space before comma and ##
    
    Signed-off-by: akodanev <akodanev@gmail.com>

--- include/common.h
+++ include/common.h
@@ -38,20 +38,20 @@
 do {									\
 	switch (type) {							\
 	case TINF:							\
-		printf("%-12s INFO  : " fmt, PID, ##__VA_ARGS__);	\
+		printf("%-12s INFO  : " fmt, PID , ##__VA_ARGS__);	\
 		printf("\n");						\
 	break;								\
 	case TDBG:							\
-		printf("%-12s DEBUG : " fmt, PID, ##__VA_ARGS__);	\
+		printf("%-12s DEBUG : " fmt, PID , ##__VA_ARGS__);	\
 		printf("\n");						\
 	break;								\
 	case TERR:							\
-		printf("%-12s ERROR : " fmt, PID, ##__VA_ARGS__);	\
+		printf("%-12s ERROR : " fmt, PID , ##__VA_ARGS__);	\
 		printf("\n");						\
 		fflush(stdout);						\
 	break;								\
 	case TERN:							\
-		printf("%-12s ERROR : " fmt, PID, ##__VA_ARGS__);	\
+		printf("%-12s ERROR : " fmt, PID , ##__VA_ARGS__);	\
 		printf(", error: %s\n", strerror(errno));		\
 		fflush(stdout);						\
 	}								\
